c49f62897645f1a970df55b1768d1832d54dd896
Ved <ved78142@gmail.com>
2022-10-13 11:14:26 +0530

    first task

---
+++ Frog1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<long long> vll;
+#define pi (3.141592653589)
+#define mod 1e9 + 7
+#define ll long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define minf(a, b, c, d) min(d, min(c, min(a, b)))
+#define rfoi(n) for (int i = n - 1; i >= 0; i--)
+#define rfoj(n) for (int j = n - 1; j >= 0; j--)
+#define foi(n) for (int i = 0; i < n; i++)
+#define foj(n) for (int j = 0; j < n; j++)
+#define printv(v)     \
+    for (auto it : v) \
+        cout << it;
+#define print(x) cout << x << endl;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long int t;
+    t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
